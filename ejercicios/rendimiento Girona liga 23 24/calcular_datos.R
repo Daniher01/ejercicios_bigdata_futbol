018065fac910a1178bb06b1402ed9c4738412b6a
Daniel Hernandez <daniher02@gmail.com>
2023-11-13 18:33:19 -0300

    se analizan los datos de la liga

--- ejercicios/rendimiento Girona liga 23 24/calcular_datos.R
+++ ejercicios/rendimiento Girona liga 23 24/calcular_datos.R
@@ -17,7 +17,7 @@ liga_23_24$ppda_allowed <- gsub("'", '"', liga_23_24$ppda_allowed)
 ppda_lista <- lapply(liga_23_24$ppda, fromJSON)
 ppda_allowed_lista <- lapply(liga_23_24$ppda_allowed, fromJSON)
 
-# Extraer valores de "att"
+# Extraer valores de "PPDA"
 att_ppda <- sapply(ppda_lista, function(x) x$att)
 def_ppda <- sapply(ppda_lista, function(x) x$def)
 
@@ -25,18 +25,25 @@ att_ppda_allowed <- sapply(ppda_allowed_lista, function(x) x$att)
 def_ppda_allowed <- sapply(ppda_allowed_lista, function(x) x$def)
 
 liga_23_24 = liga_23_24 %>%
-              mutate(ppda_att = att_ppda,
-                     ppda_def = def_ppda,
-                     ppda_allowed_att = att_ppda_allowed,
-                     ppda_allowed_def = def_ppda_allowed)
+              mutate(ppda = att_ppda/def_ppda,
+                     ppda_allowed = att_ppda_allowed/def_ppda_allowed)
 
 # GRAFICA xG a favor y xG en contra
-data_teams = liga_23_24 %>%
+team_stats = liga_23_24 %>%
           group_by(title) %>%
-          summarise(total_xg = sum(x_g),
-                    total_xg_a = sum(x_ga),
+          summarise(total_xg = sum(npx_g),
+                    total_xg_a = sum(npx_ga),
                     goals = sum(scored),
                     goals_againts = sum(missed),
-                    ppda = att_ppda/def_ppda,
-                    ppda_allowed = att_ppda_allowed/def_ppda_allowed)
+                    ppda_mean = mean(ppda),
+                    ppda_allowed_mean = mean(ppda_allowed),
+                    ocasiones_gol = mean(deep),
+                    ocaciones_gol_against = mean(deep_allowed),
+                    wins = sum(wins),
+                    draws = sum(draws),
+                    loses = sum(loses))
+
+write_csv(team_stats, 'ejercicios/rendimiento Girona liga 23 24/stats_teams.csv') 
+
+
             
